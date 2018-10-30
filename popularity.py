7897e9c940fab1019355b5f1ca8756d7dbc3b00c
Matt <MattSmelser2007@gmail.com>
2018-10-30 11:25:51 -0600

    updated popularity to better definition

--- popularity.py
+++ popularity.py
@@ -1,5 +1,8 @@
-def popularity(p): #circular definition, will never actually end
-    score = 0
-    for f in friends(p):
-        score += popularity(f)
-    return score
+def popularity(t,p): 
+    if t == 0:
+        return 1
+    else:
+        score = 0
+        for f in friends(p):
+            score += popularity(t-1, f)
+        return score
