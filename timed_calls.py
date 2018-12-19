0f5f707c566e7e31cf4806f2b3d079be91344085
Matt <MattSmelser2007@gmail.com>
2018-12-19 13:26:16 -0600

    inital commit for valid.py

--- timed_calls.py
+++ timed_calls.py
@@ -57,6 +57,11 @@ def average(numbers):
 def timedcalls(n, fn, *args):
     """Call fn(*args) repeatedly: n times if n is an int, or up to
     n seconds if n is a float; return the min, avg, and max time"""
-    # Your code here.
+    if isinstance(n, int):
+        times = [timedcall(fn, *args)[0] for _ in range(n)]
+    else:
+        times = []
+        while sum(times) < n:
+            times.append(timedcall( fn, *args)[0])
     return min(times), average(times), max(times)
 
