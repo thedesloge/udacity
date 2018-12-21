98da1d160d086968fc58e19e5cf2e731cff4e720
Matt <MattSmelser2007@gmail.com>
2018-12-21 12:08:35 -0600

    Working on subpalindrome.py, added some lines to start looping through the letters in the original string and see if we can find the palindromes.

--- subpalindrome.py
+++ subpalindrome.py
@@ -18,6 +18,26 @@
 def longest_subpalindrome_slice(text):
     "Return (i, j) such that text[i:j] is the longest palindrome in text."
     # Your code here
+    longest_found = []
+    location = 0
+    lowercase_text = text.lower()
+    if ''.join(reversed(lowercase_text)) == lowercase_text:
+        return text
+    else:
+        for letter in lowercase_text: #looping through the lowercase string
+            if letter == lowercase_text[location]: #if the letter we are looking at matches the index location we are at, make a new string
+                string = lowercase_text[location:lowercase_text.index(letter)] #the new string is from the index location to where we found a matching letter
+                if string == string[:: -1]: #check if the string is the same forward and backwards
+                    if len(string) > len(longest_found): #if it is, it's a palindrome and we need to save it if it's longer than the longest we have found already
+                        longest_found = string #add it to the longest_found list if it's the longest
+                    elif len(string) = len(longest_found): #if it's tied for the longest, we add it to the list so we have both
+                        longest_found.append(string)
+            else:
+
+        return longest_found
+
+
+
 
 
 
@@ -34,5 +54,12 @@ def test():
     return 'tests pass'
 
 
-print
-test()
+print (longest_subpalindrome_slice('racecar'))
+print (longest_subpalindrome_slice('Racecar'))
+print (longest_subpalindrome_slice('RacecarX'))
+print (longest_subpalindrome_slice('Race carr'))
+print (longest_subpalindrome_slice(''))
+print (longest_subpalindrome_slice('something rac e car going'))
+print (longest_subpalindrome_slice('xxxxx'))
+print (longest_subpalindrome_slice('Mad am I ma dam.'))
+#test()
