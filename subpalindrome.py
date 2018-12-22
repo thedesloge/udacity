fcd04d96687dfd5a4f7b2fc7cb8cc65470ac34ab
Matt <MattSmelser2007@gmail.com>
2018-12-22 12:00:51 -0600

    trying to fix this mess that I've made.  Trying to get my increments and comparisons correct

--- subpalindrome.py
+++ subpalindrome.py
@@ -25,15 +25,15 @@ def longest_subpalindrome_slice(text):
         return text
     else:
         for letter in lowercase_text: #looping through the lowercase string
-            print(lowercase_text[location])
             if letter == lowercase_text[location]: #if the letter we are looking at matches the letter in the index location we are at, make a new string
+                print(lowercase_text.index(letter))
                 string = lowercase_text[location:lowercase_text.index(letter)] #the new string is from the index location to where we found a matching letter
                 if string == string[:: -1]: #check if the string is the same forward and backwards
                     if len(string) > len(longest_found): #if it is, it's a palindrome and we need to save it if it's longer than the longest we have found already
                         longest_found = string #add it to the longest_found list if it's the longest
                     elif len(string) == len(longest_found): #if it's tied for the longest, we add it to the list so we have both
                         longest_found.append(string)
-            location += 1
+        location += 1
 
         return longest_found
 
